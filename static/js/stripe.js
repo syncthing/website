656324567e6be058627f05fd10536ef42b2eed14
Jakob Borg <jakob@kastelo.net>
2020-04-17 10:56:39 +0200

    Use Stripe backend charging function

--- static/js/stripe.js
+++ static/js/stripe.js
@@ -1,35 +1,31 @@
 var stripe = Stripe('pk_live_7SJOzG0qoGnIjMHbEOnnhEce');
-var oneTimeSKU = 'sku_FZzomJKEpaGTVk';
-var recurringSKU = 'plan_FZzqRxfQtAx34O';
-// var stripe = Stripe('pk_test_U7UN5bE4K6xwGA1nL6kjBfib');
-// var oneTimeSKU = 'sku_FZzIBb9nkPEwrd';
-// var recurringSKU = 'plan_FZzalXk9lFd7sm';
+//var stripe = Stripe('pk_test_U7UN5bE4K6xwGA1nL6kjBfib');
 
-document.getElementById('donate-once-button').addEventListener('click', function () {
-    var amount = +(document.getElementById('donation-amount').value);
-    stripe.redirectToCheckout({
-        items: [{ sku: oneTimeSKU, quantity: amount }],
-        successUrl: 'https://syncthing.net/donations/success/',
-        cancelUrl: 'https://syncthing.net/donations/cancelled/',
-        submitType: 'donate',
-    }).then(function (result) {
+function redirect(sessionID) {
+    stripe.redirectToCheckout({ sessionId: sessionID }).then(function (result) {
         if (result.error) {
             var displayError = document.getElementById('error-message');
             displayError.textContent = result.error.message;
         }
     });
+}
+
+document.getElementById('donate-once-button').addEventListener('click', function () {
+    var amount = +(document.getElementById('donation-amount').value);
+    var req = JSON.stringify({ recurring: false, count: amount });
+    $.post("/.netlify/functions/donate", req,
+        function (data) {
+            redirect(data.sessionID);
+        }, "json"
+    );
 });
 
 document.getElementById('donate-monthly-button').addEventListener('click', function () {
     var amount = +(document.getElementById('donation-amount').value);
-    stripe.redirectToCheckout({
-        items: [{ plan: recurringSKU, quantity: amount }],
-        successUrl: 'https://syncthing.net/donations/recurring/',
-        cancelUrl: 'https://syncthing.net/donations/cancelled/',
-    }).then(function (result) {
-        if (result.error) {
-            var displayError = document.getElementById('error-message');
-            displayError.textContent = result.error.message;
-        }
-    });
+    var req = JSON.stringify({ recurring: true, count: amount });
+    $.post("/.netlify/functions/donate", req,
+        function (data) {
+            redirect(data.sessionID);
+        }, "json"
+    );
 });
